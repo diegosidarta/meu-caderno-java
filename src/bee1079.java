c94332db8e2b51b6622ecb1add8ad189b76abb3e
Diego Sidarta <diegosidarta123@gmail.com>
2025-12-05 12:26:47 -0300

    Resolvendo Médias Ponderadas - exercício beecrowd 1079

---
+++ src/bee1079.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class bee1079 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i=0; i<N; i++){
+            double nota1 = sc.nextDouble();
+            double nota2 = sc.nextDouble();
+            double nota3 = sc.nextDouble();
+
+            double media = (nota1 * 2.0 + nota2 * 3.0 + nota3 * 5.0) / 10.0;
+
+            System.out.printf("%.1f%n", media);
+        }
+
+
+
+
+    }
+
+}
