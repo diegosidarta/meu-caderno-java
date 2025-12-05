56ecc8c7a2e54222cc436b7ccd62c79196406621
Diego Sidarta <diegosidarta123@gmail.com>
2025-12-05 12:49:12 -0300

    Testando contagem regressiva em for

---
+++ src/test2_for.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class test2_for {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Contagem regressiva
+        for(int i=4; i>=0; i--){
+            System.out.println("Valor de i: " + i);
+        }
+
+
+    }
+}
