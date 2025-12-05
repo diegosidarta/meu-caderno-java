fc183fa013ba930ceec69cced8b4f9368f2fa33f
Diego Sidarta <diegosidarta123@gmail.com>
2025-12-05 12:48:58 -0300

    Testando contagem em for

---
+++ src/test1_for.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class test1_for {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0; i < 5; i = i + 1){
+            System.out.println("Valor de i: " + i);
+        }
+
+    }
+}
+/*
+Resultado na tela:
+
+Valor de i: 0
+Valor de i: 1
+Valor de i: 2
+Valor de i: 3
+Valor de i: 4
+
+*/
